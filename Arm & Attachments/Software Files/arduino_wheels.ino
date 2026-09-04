58bc04f6dacabbfc708a5261f4719d6e4d28732f
Lu-Minimum <160276327+Lu-Minimum@users.noreply.github.com>
2026-09-04 15:46:44 -0400

    Update arduino_wheels.ino to remove excess definitions

--- Arm & Attachments/Software Files/arduino_wheels.ino
+++ Arm & Attachments/Software Files/arduino_wheels.ino
@@ -39,20 +39,20 @@ little wheel = 15 1/2
 
 // ================= ARM SPEED CONFIG ==================
 
-// effectorSpd: speed (RPM) of opening/closing end effector
+/*// effectorSpd: speed (RPM) of opening/closing end effector
 #define EFFECTOR_SPD 40
 // effectorEase: speed (RPM) of easing the opening/closing of the end effector
 #define EFFECTOR_EASE 20
 // effectorTimeFull: time (ms) for effector to move at full speed when opening/closing
 #define EFFECTOR_TIME_FULL 750
 // effectorTimeEase: time (ms) for effector to move at eased speed when opening/closing
-#define EFFECTOR_TIME_EASE 250
+#define EFFECTOR_TIME_EASE 250*/
 
 // baseSpd: speed (RPM) of the arm base
 #define BASE_SPD 20
 
 // armSpd: speed (RPM) of the arm shoulder & elbow
-#define ARM_SPD 20
+//#define ARM_SPD 20
 
 // =====================================================
 
